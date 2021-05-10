7958c77412c852fc776045ffc78306d999459450
Oliver Pohl <oliverpohl@datenbistro.de>
2021-05-10 23:15:25 +0200

    FG-03-08: Add simple test

--- tests/unit/components/SelectAutoComplete.vue.spec.js
+++ tests/unit/components/SelectAutoComplete.vue.spec.js
@@ -1,8 +1,11 @@
-import { shallowMount, createLocalVue } from "@vue/test-utils";
+import { shallowMount, createLocalVue, mount } from "@vue/test-utils";
+import { Platform } from 'quasar'
 import * as Vuex from "vuex";
 import VueRouter from "vue-router";
 import { routes } from "@/routes";
 import SelectAutoComplete from "@/components/SelectAutoComplete.vue";
+import { uniquePersons } from "../fixtures/unique-entities";
+import { QSelect } from "quasar";
 
 describe("SelectAutoComplete", () => {
   let localVue;
@@ -15,13 +18,17 @@ describe("SelectAutoComplete", () => {
     localVue = createLocalVue();
     localVue.use(Vuex);
 
-    wrapper = shallowMount(SelectAutoComplete, {
+    wrapper = mount(SelectAutoComplete, {
       localVue,
       store,
       router,
       props: {
         entity: "recipient",
         label: "Empfänger"
+      },
+      data() { return {
+        options: uniquePersons
+        }
       }
     });
   });
@@ -47,5 +54,9 @@ describe("SelectAutoComplete", () => {
 
     expect(wrapper.vm.model.label).toBe("");
     expect(wrapper.vm.model.value).toBe("");
+  });
+
+  it("sees the available list of unique items", () => {
+    expect(wrapper.vm.options.length).toBe(5);
   });
 });
