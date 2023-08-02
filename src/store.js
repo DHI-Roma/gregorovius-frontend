bb71fef63ac7fdeb5588436e3f1788b41b1d5f95
Oliver Pohl <oliver.pohl@strix.net>
2023-08-02 22:08:13 +0200

    DHI-GREG-FE-71: Format store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import { dataService } from './shared';
+import Vue from "vue";
+import Vuex from "vuex";
+import { dataService } from "./shared";
 
 Vue.use(Vuex);
 
@@ -24,12 +24,12 @@ export default new Vuex.Store({
     facsimiles: {},
     fullNameIndex: [],
     selectedRecipients: [],
-    selectedPlaceReceived: { label: '', value: '' },
-    selectedPlaceSent: { label: '', value: '' },
+    selectedPlaceReceived: { label: "", value: "" },
+    selectedPlaceSent: { label: "", value: "" },
     selectedYears: [],
     activeComment: {
-      id: '',
-      text: '',
+      id: "",
+      text: "",
     },
   },
   getters: {
@@ -99,16 +99,16 @@ export default new Vuex.Store({
     SET_SELECTED(state, payload) {
       const newModel = { label: state.fullNameIndex[payload.value], value: payload.value };
       switch (payload.entity) {
-        case 'recipient':
+        case "recipient":
           state.selectedRecipients = payload.value;
           break;
-        case 'placeReceived':
+        case "placeReceived":
           state.selectedPlaceReceived = newModel;
           break;
-        case 'placeSent':
+        case "placeSent":
           state.selectedPlaceSent = newModel;
           break;
-        case 'years':
+        case "years":
           state.selectedYears = payload.value;
           break;
         default:
@@ -123,98 +123,98 @@ export default new Vuex.Store({
     },
     SET_FULL_LETTERS_INDEX(state, fullLettersIndex) {
       state.fullLettersIndex = fullLettersIndex;
-    }
+    },
   },
   actions: {
     async loadFacsimilesAction({ commit }) {
-      commit('SET_LOADING_STATUS', true);
-      commit('SET_NAMED_LOADING_STATUS', { entity: 'facsimiles', loading: true });
+      commit("SET_LOADING_STATUS", true);
+      commit("SET_NAMED_LOADING_STATUS", { entity: "facsimiles", loading: true });
       const facsimiles = await dataService.getFacsimiles();
-      commit('SET_FACSIMILES', facsimiles);
+      commit("SET_FACSIMILES", facsimiles);
 
-      commit('SET_LOADING_STATUS', false);
-      commit('SET_NAMED_LOADING_STATUS', { entity: 'facsimiles', loading: false });
+      commit("SET_LOADING_STATUS", false);
+      commit("SET_NAMED_LOADING_STATUS", { entity: "facsimiles", loading: false });
     },
     async loadLettersAction({ commit }) {
       if (this.getters.letters.length === 0 && !this.getters.loadingStatusLetters) {
-        commit('SET_LOADING_STATUS', true);
-        commit('SET_NAMED_LOADING_STATUS', { entity: 'letters', loading: true });
+        commit("SET_LOADING_STATUS", true);
+        commit("SET_NAMED_LOADING_STATUS", { entity: "letters", loading: true });
         const letters = await dataService.getLetters();
-        commit('GET_LETTERS', letters);
-        commit('SET_LOADING_STATUS', false);
-        commit('SET_NAMED_LOADING_STATUS', { entity: 'letters', loading: false });
+        commit("GET_LETTERS", letters);
+        commit("SET_LOADING_STATUS", false);
+        commit("SET_NAMED_LOADING_STATUS", { entity: "letters", loading: false });
       }
     },
     async loadPersonsAction({ commit }) {
       if (this.getters.persons.length === 0 && !this.getters.loadingStatusPersons) {
-        commit('SET_LOADING_STATUS', true);
-        commit('SET_NAMED_LOADING_STATUS', { entity: 'persons', loading: true });
-        const persons = await dataService.getEntities('persons');
-        commit('GET_PERSONS', persons);
-        commit('SET_LOADING_STATUS', false);
-        commit('SET_NAMED_LOADING_STATUS', { entity: 'persons', loading: false });
+        commit("SET_LOADING_STATUS", true);
+        commit("SET_NAMED_LOADING_STATUS", { entity: "persons", loading: true });
+        const persons = await dataService.getEntities("persons");
+        commit("GET_PERSONS", persons);
+        commit("SET_LOADING_STATUS", false);
+        commit("SET_NAMED_LOADING_STATUS", { entity: "persons", loading: false });
       }
     },
     async loadPlacesAction({ commit }) {
       if (this.getters.places.length === 0 && !this.getters.loadingStatusPlaces) {
-        commit('SET_LOADING_STATUS', true);
-        commit('SET_NAMED_LOADING_STATUS', { entity: 'places', loading: true });
-        const places = await dataService.getEntities('places');
-        commit('GET_PLACES', places);
-        commit('SET_LOADING_STATUS', false);
-        commit('SET_NAMED_LOADING_STATUS', { entity: 'places', loading: false });
+        commit("SET_LOADING_STATUS", true);
+        commit("SET_NAMED_LOADING_STATUS", { entity: "places", loading: true });
+        const places = await dataService.getEntities("places");
+        commit("GET_PLACES", places);
+        commit("SET_LOADING_STATUS", false);
+        commit("SET_NAMED_LOADING_STATUS", { entity: "places", loading: false });
       }
     },
     async loadWorksAction({ commit }) {
       if (this.getters.works.length === 0 && !this.loadingStatusWorks) {
-        commit('SET_LOADING_STATUS', true);
-        commit('SET_NAMED_LOADING_STATUS', { entity: 'works', loading: true });
-        const works = await dataService.getEntities('works');
-        commit('GET_WORKS', works);
-        commit('SET_LOADING_STATUS', false);
-        commit('SET_NAMED_LOADING_STATUS', { entity: 'works', loading: false });
+        commit("SET_LOADING_STATUS", true);
+        commit("SET_NAMED_LOADING_STATUS", { entity: "works", loading: true });
+        const works = await dataService.getEntities("works");
+        commit("GET_WORKS", works);
+        commit("SET_LOADING_STATUS", false);
+        commit("SET_NAMED_LOADING_STATUS", { entity: "works", loading: false });
       }
     },
     async loadFullNameIndexAction({ commit }) {
-      await Promise.all([this.dispatch('loadPersonsAction'), this.dispatch('loadPlacesAction')]);
+      await Promise.all([this.dispatch("loadPersonsAction"), this.dispatch("loadPlacesAction")]);
       if (Object.entries(this.getters.fullNameIndex).length === 0) {
-        commit('SET_LOADING_STATUS', true);
+        commit("SET_LOADING_STATUS", true);
         let fullNameIndex = {};
         const entities = [...this.getters.persons, ...this.getters.places];
         entities.map(async (entity) => {
           const targetEntity = entities.find((item) => item.id === entity.id);
           fullNameIndex[entity.id] =
-            targetEntity.properties.name.toponym || targetEntity.properties.name.fullName || 'NN';
+            targetEntity.properties.name.toponym || targetEntity.properties.name.fullName || "NN";
         });
-        commit('GET_FULLNAME_INDEX', fullNameIndex);
-        commit('SET_LOADING_STATUS', false);
+        commit("GET_FULLNAME_INDEX", fullNameIndex);
+        commit("SET_LOADING_STATUS", false);
       }
     },
 
     async loadEntitiesAction() {
       const dispatchers = [];
       if (this.getters.letters.length === 0) {
-        dispatchers.push(this.dispatch('loadLettersAction'));
+        dispatchers.push(this.dispatch("loadLettersAction"));
       }
 
       if (this.getters.persons.length === 0) {
-        dispatchers.push(this.dispatch('loadPersonsAction'));
+        dispatchers.push(this.dispatch("loadPersonsAction"));
       }
 
       if (this.getters.places.length === 0) {
-        dispatchers.push(this.dispatch('loadPlacesAction'));
+        dispatchers.push(this.dispatch("loadPlacesAction"));
       }
 
       if (this.getters.works.length === 0) {
-        dispatchers.push(this.dispatch('loadWorksAction'));
+        dispatchers.push(this.dispatch("loadWorksAction"));
       }
 
       if (Object.keys(this.getters.facsimiles).length === 0) {
-        dispatchers.push(this.dispatch('loadFacsimilesAction'));
+        dispatchers.push(this.dispatch("loadFacsimilesAction"));
       }
 
       if (!this.getters.fullLettersIndex) {
-        dispatchers.push(this.dispatch('loadFullLettersIndexAction'));
+        dispatchers.push(this.dispatch("loadFullLettersIndexAction"));
       }
 
       await Promise.allSettled(dispatchers);
@@ -225,32 +225,32 @@ export default new Vuex.Store({
         return;
       }
 
-      commit('SET_LOADING_STATUS', true);
-      commit('SET_NAMED_LOADING_STATUS', { entity: 'fullLettersIndex', loading: true});
+      commit("SET_LOADING_STATUS", true);
+      commit("SET_NAMED_LOADING_STATUS", { entity: "fullLettersIndex", loading: true });
       const index = await dataService.getFullLettersIndex();
-      commit('SET_FULL_LETTERS_INDEX', index);
-      commit('SET_NAMED_LOADING_STATUS', { entity: 'fullLettersIndex', loading: false});
-      commit('SET_LOADING_STATUS', false);
+      commit("SET_FULL_LETTERS_INDEX", index);
+      commit("SET_NAMED_LOADING_STATUS", { entity: "fullLettersIndex", loading: false });
+      commit("SET_LOADING_STATUS", false);
     },
 
     async setSelectedAction({ commit }, payload) {
-      commit('SET_SELECTED', payload);
+      commit("SET_SELECTED", payload);
     },
 
     async setLoadingStatus({ commit }, value) {
-      commit('SET_LOADING_STATUS', value);
+      commit("SET_LOADING_STATUS", value);
     },
 
     async setActiveComment({ commit }, comment) {
-      commit('SET_ACTIVE_COMMENT', comment);
+      commit("SET_ACTIVE_COMMENT", comment);
     },
 
     async unselectComment({ commit }) {
-      commit('SET_ACTIVE_COMMENT', {});
+      commit("SET_ACTIVE_COMMENT", {});
     },
 
     async setLettersFiltered({ commit }, letters) {
-      commit('SET_LETTERS_FILTERED', letters);
+      commit("SET_LETTERS_FILTERED", letters);
     },
   },
 });
