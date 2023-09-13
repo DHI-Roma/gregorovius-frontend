e220fd29e84c21d61d2cf56a4cdd33247de91bd8
Oliver Pohl <oliver.pohl@strix.net>
2023-09-13 20:33:40 +0200

    DHI-GREG-FE-76: Adjust sorting algorithm to make letter index find work for chromium based browsers

---
+++ src/services/sort-service.js
@@ -0,0 +1,11 @@
+export const sortLetterByDate  = (letterA, letterB) => {
+  if (letterA.properties.date > letterB.properties.date) {
+    return 1;
+  }
+
+  if (letterA.properties.date < letterB.properties.date) {
+    return -1;
+  }
+
+  return 0;
+}
